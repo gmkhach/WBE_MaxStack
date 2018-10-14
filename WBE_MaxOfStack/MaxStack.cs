ef717e492abde657c41d1a7fb0239f11eac3238b
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-13 21:29:26 -0400

    fixed the prompting messages

--- WBE_MaxOfStack/MaxStack.cs
+++ WBE_MaxOfStack/MaxStack.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* 
+ * Use the built-in Stack class to implement a new class MaxStack with a method GetMax() that returns the largest element in the stack.
+ * GetMax() should not remove the item.
+ * Your stacks will contain only integers.
+ */
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- WBE_MaxOfStack/Program.cs
+++ WBE_MaxOfStack/Program.cs
@@ -1,13 +1,15 @@
-﻿/* Use the built-in Stack class to implement a new class MaxStack with a method GetMax() that returns the largest element in the stack.
+﻿/* 
+ * Use the built-in Stack class to implement a new class MaxStack with a method GetMax() that returns the largest element in the stack.
  * GetMax() should not remove the item.
  * Your stacks will contain only integers.
  */
+
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections;
 
 namespace WBE_MaxOfStack
 {
@@ -19,7 +21,7 @@ namespace WBE_MaxOfStack
             {
                 try
                 {
-                    Console.Write("\nEnter an integer between 1 and 100\n\n>>> ");
+                    Console.Write("\nEnter integers to create a stack\n\n>>> ");
                     int[] input = Console.ReadLine().Split(',').Select(x => int.Parse(x)).ToArray();
                     MaxStack myStack = new MaxStack();
                     foreach (var x in input)
@@ -32,7 +34,7 @@ namespace WBE_MaxOfStack
                 {
                     Console.WriteLine("\n" + ex.Message);
                 }
-                Console.Write("\nPress Enter to try another floor...");
+                Console.Write("\nPress Enter to try another stack...");
                 Console.ReadLine();
                 Console.Clear();
             } while (true);
